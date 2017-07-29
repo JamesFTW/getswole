faddb7ee0572fdb470e778de67ef73fc853bb629
James Andrews <jamesftw@Jamess-MacBook-Pro.local>
2017-07-28 23:52:47 -0700

    removed more stuff from index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,22 +1,7 @@
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-  View
-} from 'react-native';
-
-import Navbar         from './app/components/navbar.js'
-import Workout        from './app/components/Workout.js'
+import {AppRegistry} from 'react-native';
 import WorkoutScreen  from './app/screens/WorkoutScreen.js'
-
-import { NativeRouter, Route, Link } from 'react-router-native'
-
-let data = require('./sample.json')
 
 export default class getswole extends Component {
   render() {
