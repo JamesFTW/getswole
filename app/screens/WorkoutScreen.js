62214e7a3c31fd1291bf180fbba064cd04cd104c
James Andrews <jamesftw@Jamess-MacBook-Pro.local>
2017-07-28 23:27:41 -0700

    added WorkoutScreen.js

---
+++ app/screens/WorkoutScreen.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import Navbar         from '../components/navbar.js'
+import Workout        from '../components/Workout.js'
+
+import { NativeRouter, Route, Link } from 'react-router-native'
+
+let data = require('../../sample.json')
+
+export default class WorkoutScreen extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      workouts: data.exercises,
+      workoutName: data.workout
+      }
+  }
+  render() {
+    return (
+    <View style={styles.container2}>
+      <View style={styles.flexContainer}>
+
+        <View style={styles.flexHeader}>
+          <Image style={styles.image} source={require('../../img/header/header.png')}>
+            <Text style={styles.header}>{this.state.workoutName}</Text>
+          </Image>
+        </View>
+
+        <ScrollView style={styles.flexBody}>
+          {this.state.workouts.map((exercise, i) => {
+            return (
+              <View key={i}>
+                <Workout id={exercise.id} type={exercise.type} weight={exercise.weight} sets={exercise.sets} rep={exercise.rep}/>
+              </View>
+            )
+          })}
+        </ScrollView>
+
+        <View style={styles.flexFooter}>
+          <Navbar />
+        </View>
+
+      </View>
+    </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  flexBody:{
+    flex: 1,
+    display: 'flex',
+    backgroundColor: '#EDEDED'
+  },
+  flexFooter: {
+    backgroundColor: 'red'
+  },
+  image:{
+    backgroundColor: 'transparent'
+  },
+  container2: {
+    position: 'absolute',
+    top: 20,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  flexContainer:{
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%'
+  },
+  header:{
+    zIndex: 2,
+    textAlign: 'center',
+    top: 11,
+    fontSize: 24,
+    fontFamily: 'HelveticaNeue-Thin',
+    letterSpacing: 3,
+    color: '#FFFFFF',
+    fontWeight: "300"
+  }
+});
