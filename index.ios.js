434792b2025b630ee8c5cc3c6b5c707e56e87e66
James Andrews <bdaddyja@gmail.com>
2017-07-30 21:34:48 -0700

    index.ios.js now reads routes

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,15 @@
 
 import React, { Component } from 'react';
-import {AppRegistry} from 'react-native';
-import WorkoutScreen  from './app/screens/WorkoutScreen.js'
+import {AppRegistry}        from 'react-native';
+import WorkoutScreen        from './app/screens/WorkoutScreen.js'
+import routes               from './app/routes/routes.js'
 
 export default class getswole extends Component {
   render() {
     return (
-      <WorkoutScreen />
+      <routes />
     );
   }
 }
 
-AppRegistry.registerComponent('getswole', () => getswole);
+AppRegistry.registerComponent('getswole', () => routes);
