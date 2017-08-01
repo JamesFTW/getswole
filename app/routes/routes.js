aaae6dae396ac6b59eba27a1e5a11b70f3107644
James Andrews <bdaddyja@gmail.com>
2017-07-31 20:58:18 -0700

    formmating changes and added a switch

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,13 +1,16 @@
-import React                         from 'react'
-import {View}                        from 'react-native';
-import { NativeRouter, Route, Link } from 'react-router-native'
-import WorkoutScreen                 from '../screens/WorkoutScreen.js'
-import ProfileScreen                 from '../screens/ProfileScreen.js'
+import React                          from 'react'
+import {View}                         from 'react-native';
+import { NativeRouter, Route, Switch} from 'react-router-native'
+import WorkoutScreen                  from '../screens/WorkoutScreen.js'
+import ProfileScreen                  from '../screens/ProfileScreen.js'
 
 
 const routes = () => (
   <NativeRouter>
+    <Switch>
       <Route exact path="/" component={WorkoutScreen}/>
+      <Route path="/ProfileScreen" component={ProfileScreen}/>
+    </Switch>
   </NativeRouter>
 )
 
