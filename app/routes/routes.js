1f9c5ab370eaa53cbac3c4ba9c79d7b5942fb312
James Andrews <bdaddyja@gmail.com>
2017-08-17 23:53:00 -0700

    fixed navbar route forreal this time.:

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,17 +1,70 @@
-import React                                      from 'react'
-import {View}                                     from 'react-native';
-import {NativeRouter, Route, Switch,hashHistory}  from 'react-router-native'
-import WorkoutScreen                              from '../screens/WorkoutScreen.js'
-import ProfileScreen                              from '../screens/ProfileScreen.js'
-import Navbar                                     from '../components/navbar.js'
+import React                                                  from 'react'
+import {View, StyleSheet, ImageBackground, Text, ScrollView}  from 'react-native';
+import {NativeRouter, Route, Switch,hashHistory}              from 'react-router-native'
+import WorkoutScreen                                          from '../screens/WorkoutScreen.js'
+import ProfileScreen                                          from '../screens/ProfileScreen.js'
+import Navbar                                                 from '../components/navbar.js'
+import Layout                                                 from '../containers/Layout.js'
 
 const routes = () => (
   <NativeRouter history={hashHistory}>
-    <Switch>
-      <Route exact path="/" component={WorkoutScreen} overlayComponent={Navbar}/>
-      <Route path="/ProfileScreen" component={ProfileScreen} overlayComponent={Navbar}/>
-    </Switch>
+    <View style={styles.container2}>
+    <View style={styles.flexContainer}>
+
+      {/* <View style={styles.flexHeader}>
+        <ImageBackground style={styles.image} source={require('../../img/header/header.png')}>
+          <Text style={styles.header}></Text>
+        </ImageBackground>
+      </View> */}
+
+      <ScrollView style={styles.flexBody}>
+        <View>
+
+        </View>
+      </ScrollView>
+
+      <Route exact path="/" component={WorkoutScreen}/>
+      <Route path="/ProfileScreen" component={ProfileScreen}/>
+      <View style={styles.flexFooter}>
+        <Navbar />
+      </View>
+    </View>
+  </View>
   </NativeRouter>
 )
-
+const styles = StyleSheet.create({
+  flexBody:{
+    flex: 1,
+    display: 'flex',
+    backgroundColor: '#EDEDED'
+  },
+  container2: {
+    position: 'absolute',
+    top: 20,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  flexContainer:{
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%'
+  },
+  flexFooter: {
+    backgroundColor: 'red',
+  },
+  flexHeader:{
+    height: '5%'
+  },
+  header:{
+    zIndex: 2,
+    textAlign: 'center',
+    top: 11,
+    fontSize: 24,
+    fontFamily: 'HelveticaNeue-Thin',
+    letterSpacing: 3,
+    color: '#FFFFFF',
+    fontWeight: "300"
+  }
+})
 export default routes
