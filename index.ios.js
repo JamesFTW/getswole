d64b6785b9f87ae7d30a29b6ba3b5e9e634f908b
James Andrews <bdaddyja@gmail.com>
2017-10-02 14:47:35 -0700

    removed workoutscreen import

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react';
 import {AppRegistry}        from 'react-native';
-import WorkoutScreen        from './app/screens/WorkoutScreen.js'
 import routes               from './app/routes/routes.js'
 
 export default class getswole extends Component {
