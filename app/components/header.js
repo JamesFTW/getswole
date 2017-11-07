f68c19085898de65056cdbfebc5329ac0184d1bc
James Andrews <bdaddyja@gmail.com>
2017-11-06 23:03:23 -0800

    added header.js

---
+++ app/components/header.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  TouchableHighlight,
+} from 'react-native';
+import {NativeRouter, Link}   from 'react-router-native'
+
+export default class Header extends Component{
+  render(){
+    return(
+      <View style={styles.container2}>
+        <View style={styles.flexContainer}>
+
+          <View style={styles.flexHeader}>
+            <ImageBackground style={styles.image} source={require('../../img/header/header.png')}/>
+          </View>
+
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  flexFooter: {
+    backgroundColor: 'red'
+  },
+  container2: {
+    position: 'absolute',
+  },
+  flexBody:{
+    flex: 1,
+    display: 'flex',
+    backgroundColor: '#EDEDED'
+  },
+  flexHeader:{
+    height: '5%'
+  },
+  flexContainer:{
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%'
+  },
+  image:{
+    backgroundColor: 'transparent'
+  },
+})
