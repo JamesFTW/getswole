47d885f93821bea208a8e6b7a804317025b69298
James Andrews <bdaddyja@gmail.com>
2017-11-20 08:02:58 -0800

    add /users/:userid route + some es6 refactoring

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app  = express()
 const port = process.env.PORT || 3000
 
 app.use(helmet())
-pg.defaults.ssl = true;
+pg.defaults.ssl = true
 
 app.get('/', function (req, res) {
   res.send('Hello World! \n' + process.env.DATABASE_URL)
@@ -26,32 +26,34 @@ app.get('/', function (req, res) {
 app.get('/db', function(req, res){
   db.any("SELECT * FROM exercise")
     .then(function (data) {
-        console.log(data);
+        console.log(data)
         res.send(JSON.stringify(data))
     })
     .catch(function (error) {
-        console.log("ERROR:", error);
-    });
-})
-
-app.get('/user', function(req, res){
-  db.any("SELECT * FROM _user")
-    .then(function (data) {
-        console.log(JSON.stringify(data[0]));
-        res.send(JSON.stringify(data[0]))
+        console.log("ERROR:", error)
     })
-    .catch(function (error){
-        console.log("ERROR:", error);
-    });
 })
+app.get('/users', (req, res) => {
+  db.any("SELECT * FROM _user")
+    .then(data => {
+      console.log(JSON.stringify(data))
+      res.send(JSON.stringify(data))
+    })
+    .catch(error => console.log("ERROR:", error))
+  })
 
-// app.get(`/user/${userid}`, function(req, res){
-//   db.any(`SELECT * FROM _user WHEN user_id=${userid}`)
-//   .then(function(data){
-//     console.log(JSON.stringify(data[0]))
-//     res.send(JSON.stringify(data[0]))
-//   })
-// })
+app.get('/users/:user_id', (req, res) => {
+  db.any("SELECT * FROM _user")
+    .then(data => {
+      for(let value of data){
+        if(value.user_id == req.params.user_id){
+          res.send(JSON.stringify(value))
+        }
+        //figure out how to throw 404 if no id's match
+      }
+    })
+    .catch(error => console.log("ERROR:", error))
+  })
 
 app.get('/plan/id', function(req, res){
   db.any("select * from plan where planid='0cd96b6a-f4b8-4780-8d63-8803082361f9'")
@@ -63,7 +65,7 @@ app.get('/plan/id', function(req, res){
     .catch(function(error){
       console.log("Error: ", error)
     })
-})
+  })
 
 app.listen(port, function () {
   console.log("Express server listening on port: " + port)
