006fccc36b6545007abcea82dee25386146003e7
James Andrews <bdaddyja@gmail.com>
2019-02-09 02:20:47 -0800

    Init server with passport and sessions.

--- api/index.js
+++ api/index.js
@@ -3,12 +3,21 @@ if (process.env.NODE_ENV === 'development') {
   require('dotenv').config()
 }
 
-const express = require('express')
-const pg      = require('pg')
-const pgp     = require("pg-promise")()
-const db      = pgp(process.env.DATABASE_URL)
-const helmet  = require('helmet')
+const express           = require('express')
+const sessionMiddleware = require('../config/session.js')
+const helmet            = require('helmet')
+const endpoints         = require('./endpoints')
+const { passport }      = require('./auth/passport.js')
+const app               = express()
+const port              = process.env.PORT || 3000
 
-// init server
-const app  = express()
-const port = process.env.PORT || 3000
+app.use(helmet())
+app.use(sessionMiddleware)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use('/api', endpoints)
+
+
+app.listen(port, '127.0.0.1', () => console.log(`listening on ${port}`))
