a148f8f332db210262ae59756a6c39ba120f455c
James Andrews <bdaddyja@gmail.com>
2019-02-15 02:43:06 -0800

    Clean up

--- app/screens/profileScreen.js
+++ app/screens/profileScreen.js
@@ -1,22 +1,22 @@
+
 import React, { Component } from 'react'
+import BackGroundWrapper    from '../components/backGroundWrapper.js'
+import ProfileHeader        from '../components/profileHeader.js'
+import FollowButton         from '../containers/followButton.js'
+import UserFirstName        from '../components/userFirstName.js'
+import UserContainer        from '../components/userContainer.js'
+import FollowingContainer   from '../components/followingContainer.js'
+import ProfilePhoto         from '../components/profilePhoto.js'
+import Following            from '../components/following.js'
+import Followers            from '../components/followers.js'
+import CompletedWorkouts    from '../components/completedWorkout.js'
+import ScrollContent        from '../components/scrollContent.js'
+import WorkoutStatus        from '../components/workoutStatus.js'
 
 import {
   View,
   ActivityIndicator
 } from 'react-native'
-
-import BackGroundWrapper  from '../components/backGroundWrapper.js'
-import ProfileHeader      from '../components/profileHeader.js'
-import FollowButton       from '../components/followButton.js'
-import UserFirstName      from '../components/userFirstName.js'
-import UserContainer      from '../components/userContainer.js'
-import FollowingContainer from '../components/followingContainer.js'
-import ProfilePhoto       from '../components/profilePhoto.js'
-import Following          from '../components/following.js'
-import Followers          from '../components/followers.js'
-import CompletedWorkouts  from '../components/completedWorkout.js'
-import ScrollContent      from '../components/scrollContent.js'
-import WorkoutStatus      from '../components/workoutStatus.js'
 
 let data = require('../completedworkout.json')
 const me = require('../assets/img/me.jpg')
@@ -37,17 +37,27 @@ export default class ProfileScreen extends Component {
     })
   }
 
-  //loop through following users list.
-  //if id is in there following is set to true
-  //if your own profile following has to change
-
   render() {
-    const { isLoading } = this.state
-    console.log(this.state)
+    const { isLoading, status } = this.state
 
-    if(isLoading) {
-      return <ActivityIndicator size="large"/>
+    if (isLoading) {
+      return <ActivityIndicator size="large" />
     }
+
+    const CompletedWorkoutDetails = status.map((exercise, i) => {
+      return (
+        <View key={i}>
+          <WorkoutStatus 
+            key={i} 
+            completed={exercise.completed} 
+            id={exercise.statusID} 
+            date={exercise.date} 
+            workout={exercise.workout}
+          />
+        </View>
+      )
+    })
+    
     return (
       <BackGroundWrapper>
         <ProfileHeader userName={"Jamesftw"}/>
@@ -59,19 +69,12 @@ export default class ProfileScreen extends Component {
               <Following following={0} />
               <Followers followers={0} />
               <View style={{right: 5, top: 3}}>
-                <FollowButton following={true} />
+                <FollowButton/>
               </View>
             </FollowingContainer>
           </UserContainer>
           <CompletedWorkouts workouts={100} totalWeight={`100k`}/>
-          {this.state.status.map((exercise, i) => {
-              return (
-                <View key={i}>
-                  <WorkoutStatus key={i} completed={exercise.completed} id={exercise.statusID} date={exercise.date} workout={exercise.workout}/>
-                </View>
-              )
-            })
-          }
+          {CompletedWorkoutDetails}
         </ScrollContent>
       </BackGroundWrapper>
     )
