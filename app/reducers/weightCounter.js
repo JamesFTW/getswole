8f957d3bd2d13c90b5157a7743984bc3c79c343c
James Andrews <bdaddyja@gmail.com>
2019-02-14 22:50:21 -0800

    Move weightCounter reducer to own file and use combineReducers.

---
+++ app/reducers/weightCounter.js
@@ -0,0 +1,44 @@
+
+import data from '../sample.json'
+import {
+  INCREMENT,
+  DECREMENT
+} from '../actions'
+
+let obj = {}
+
+const workout = data.exercises
+
+workout.map((exercises) => {
+  obj[exercises.id] = exercises
+})
+
+const node = (state = {}, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        weight: state.weight + 1
+      }
+    case DECREMENT:
+      return {
+        ...state,
+        weight: state.weight - 1
+      }
+    default:
+      return state
+  }
+}
+
+export default (state = obj, action) => {
+  const { nodeId } = action
+
+  if (typeof nodeId === 'undefined') {
+    return state
+  }
+
+  return {
+    ...state,
+    [nodeId]: node(state[nodeId], action)
+  }
+}
