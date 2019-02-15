37df7d2ac9878dd7ffd641876214d7434f3b6f00
James Andrews <bdaddyja@gmail.com>
2019-02-15 02:39:08 -0800

    Added followReducer to rootReducer.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import weightCounter from './weightCounter.js'
+import followReducer from './followReducer.js'
 
 export const rootReducer = combineReducers({
-  weightCounter
+  weightCounter,
+  followReducer
 })
