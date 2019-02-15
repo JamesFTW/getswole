de76eda65dec06b97f676a7ebcbfc6f40547e3fa
James Andrews <bdaddyja@gmail.com>
2019-02-15 02:39:41 -0800

    Added followReducer.js

---
+++ app/reducers/followReducer.js
@@ -0,0 +1,29 @@
+
+import { TOGGLE_FOLLOW } from '../actions'
+
+const node = (state = {isFollowing: false}, action) => {
+  switch (action.type) {
+    case TOGGLE_FOLLOW:
+      return {
+        ...state,
+        isFollowing: !state.isFollowing
+      }
+    default:
+      return state
+  }
+}
+
+export default (state, action) => {
+  const { type } = action
+
+  if (type === 'TOGGLE_FOLLOW') {
+    return {
+      ...state,
+      isFollowing: node(!state.isFollowing, type)
+    }
+  }
+
+  return {
+    ...state
+  }
+}
