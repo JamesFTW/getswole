63f0a5445d7d94fd1b2ca7a551f87f2f685c2456
James Andrews <bdaddyja@gmail.com>
2019-02-15 02:43:27 -0800

    Clean up

--- app/screens/workoutScreen.js
+++ app/screens/workoutScreen.js
@@ -1,14 +1,13 @@
 
 import React, { Component } from 'react'
+import BackGroundWrapper    from '../components/backGroundWrapper.js'
+import Header               from '../components/header.js'
+import ConnectedNode        from '../containers/workout.js'
+import data                 from '../sample.json'
 
 import {
   ScrollView
 } from 'react-native'
-
-import BackGroundWrapper from '../components/backGroundWrapper.js'
-import Header            from '../components/header.js'
-import ConnectedNode     from '../containers/workout.js'
-import data              from '../sample.json'
 
 export default class WorkoutScreen extends Component {
   constructor(props) {
@@ -30,7 +29,6 @@ export default class WorkoutScreen extends Component {
           sets={exercise.sets}
           rep={exercise.rep}
           key={i+1}
-          weight={0}
         />
       )
     })
