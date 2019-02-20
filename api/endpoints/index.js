6174260b3ec257715fc65fe505c6988b1d131150
James Andrews <bdaddyja@gmail.com>
2019-02-20 14:46:57 -0800

    Import workout endpoint.

--- api/endpoints/index.js
+++ api/endpoints/index.js
@@ -2,8 +2,10 @@
 const express = require('express')
 const router  = express.Router()
 
-const login = require('./login')
+const login   = require('./login')
+const workout = require('./workout')
 
 router.use('/login', login)
+router.use('/workout', workout)
 
 module.exports = router
