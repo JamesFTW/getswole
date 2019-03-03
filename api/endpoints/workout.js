a43b64c8955ee5ede8d2617131b2cc1e3c3c1b5a
James Andrews <bdaddyja@gmail.com>
2019-03-03 06:24:23 -0800

    get all workoutplans.

--- api/endpoints/workout.js
+++ api/endpoints/workout.js
@@ -3,3 +3,11 @@ const express = require('express')
 const router  = express.Router()
 
 const { db } = require('../db')
+
+router.get('/', (req, res) => {
+  db.one("select * from workoutplans")
+    .then(data => res.send(data))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
