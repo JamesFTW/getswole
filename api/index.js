fa91c979a403baf0f89fc1da27f05d78c92c180f
James Andrews <bdaddyja@gmail.com>
2019-03-03 09:05:27 -0800

    fix please

--- api/index.js
+++ api/index.js
@@ -20,6 +20,10 @@ app.use(passport.session())
 app.use(cookieParser)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.get('/', (req, res) => {
+  res.send('Hello World! \n')
+})
 app.use('/api', endpoints)
+
 
 app.listen(port, '127.0.0.1', () => console.log(`listening on ${port}`))
