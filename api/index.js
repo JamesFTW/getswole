e1a66c022a0e1f61388efcea76eba78325c8b72b
James Andrews <bdaddyja@gmail.com>
2019-03-03 09:07:59 -0800

    PLLEEASE

--- api/index.js
+++ api/index.js
@@ -20,10 +20,7 @@ app.use(passport.session())
 app.use(cookieParser)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  res.send('Hello World! \n')
-})
 app.use('/api', endpoints)
 
 
-app.listen(port, '127.0.0.1', () => console.log(`listening on ${port}`))
+app.listen(port, () => console.log(`listening on ${port}`))
