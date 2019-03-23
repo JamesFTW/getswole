556c8c8ea0460d8fda9eb71185a8a29d0500e30f
James Andrews <bdaddyja@gmail.com>
2019-03-23 09:16:54 -0700

    Center of screen component.

---
+++ app/components/centerOfScreen.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react'
+
+import {
+  StyleSheet,
+  View
+} from 'react-native'
+
+export default class CenterOfScreen extends PureComponent {
+  render() {
+    const { children } = this.props
+    
+    return (
+      <View style={styles.container}>
+        {children}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 60
+  }
+})
