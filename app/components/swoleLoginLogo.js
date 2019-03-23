edc8051e27cf97cc30e1652e8698d0142b10a997
James Andrews <bdaddyja@gmail.com>
2019-03-23 15:29:32 -0700

    Added swole login logo to component.

---
+++ app/components/swoleLoginLogo.js
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react'
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native'
+
+const DUMBBELL = 'loginIcon'
+
+export default class SwoleLoginLogo extends PureComponent {
+  render() {
+
+    return (
+      <View style={styles.buttonContainer}>
+        <Image source={{ uri: DUMBBELL }} style={styles.checkIcon}/>
+        <Text style={styles.text}>
+          SWOLE
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontFamily: 'HelveticaNeue-Bold',
+    letterSpacing: 5,
+    color: '#FFFFFF',
+    fontWeight: '700'
+  },
+  checkIcon: {
+    height: 96 / 1.5,
+    width: 111 / 1.5,
+    marginBottom: 20,
+    position: 'relative'
+  },
+  buttonContainer: {
+    position: 'relative',
+    alignItems: 'center',
+    marginLeft: 18
+  }
+})
