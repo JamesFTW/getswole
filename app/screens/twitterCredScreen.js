44d270c2d802f46191cccd7b8aa5e3da54edf6eb
James Andrews <bdaddyja@gmail.com>
2019-03-25 13:15:41 -0700

    stop login/profile webview.

---
+++ app/screens/twitterCredScreen.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import { WebView } from "react-native-webview"
+
+export default class TwitterCredScreen extends PureComponent {
+  //Might have to open in browser instead of webview
+  webview = null
+  handleWebViewNavigationStateChange = newNavState => {
+
+    const { url } = newNavState;
+    if (!url) return
+
+    // handle certain doctypes
+    if (url.includes('.pdf')) {
+      this.webview.stopLoading();
+      // open a modal with the PDF viewer
+    }
+
+    // one way to handle a successful form submit is via query strings
+    if (url.includes('/login/profile')) {
+      this.webview.stopLoading();
+      console.log(this.webview)
+      // maybe close this view?
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <View style={styles.square}></View>
+        <WebView
+          ref={ref => (this.webview = ref)}
+          javaScriptEnabled={true}
+          source={{ uri: 'https://swole.herokuapp.com/api/login/twitter' }}
+          onNavigationStateChange={this.handleWebViewNavigationStateChange}
+          onMessage={event => console.log('Received: ', event)}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   height: '100%', 
+   position: 'relative', 
+   zIndex: 102302302,
+   marginTop: 20
+  }
+})
