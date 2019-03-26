523200c25ff31ffccbeb35e0ead7d2ada1d3e41d
James Andrews <bdaddyja@gmail.com>
2019-03-26 16:15:50 -0700

    Passed login function as prop.

--- app/components/twitterFollowButton.js
+++ app/components/twitterFollowButton.js
@@ -12,9 +12,12 @@ const TWITTER_LOGO = 'twitterlogo'
 
 export default class TwitterFollowButton extends PureComponent {
   render() {
+    const { login } = this.props
 
     return (
-      <TouchableOpacity style={styles.buttonContainer}>
+      <TouchableOpacity 
+        login={() => login()} 
+        style={styles.buttonContainer}>
         <View style={styles.button}>
           <Image source={{ uri: TWITTER_LOGO }} style={styles.checkIcon} />
           <Text style={styles.title}>Log in with Twitter</Text>
@@ -32,8 +35,6 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
-    // paddingRight: '28%',
-    // paddingLeft: 10,
   },
   title: {
     fontSize: 18,
@@ -47,12 +48,12 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingLeft: 20,
     paddingRight: 20,
-    marginBottom: 60
+    marginBottom: 90
   },
   checkIcon: {
+    position: 'absolute',
     height: 32,
     width: 32,
-    position: 'absolute',
     left: 0,
     marginLeft: 15
   }
--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,25 +1,27 @@
 
 import React, { PureComponent } from 'react'
-import LoginWrapper   from '../components/loginWrapper.js'
-import CenterOfScreen from '../components/centerOfScreen.js'
-import TwitterFollowButton from '../components/twitterFollowButton.js'
-import SwoleLoginLogo from '../components/swoleLoginLogo.js'
-
-import {
-  Text,
-  StyleSheet,
-  Image,
-  View
-} from 'react-native'
+import LoginWrapper             from '../components/loginWrapper.js'
+import CenterOfScreen           from '../components/centerOfScreen.js'
+import TwitterFollowButton      from '../components/twitterFollowButton.js'
+import SwoleLoginLogo           from '../components/swoleLoginLogo.js'
 
 export default class LoginScreen extends PureComponent {
+  componentDidMount() {
+    console.log('test')
+    //fetch API to check if person already logged in via asyncstorage
+  }
+  twitterLogin = () => {
+    fetch('https://swole.herokuapp.com/api/login/twitter')
+      .then(res => console.log(res.url))
+      .catch(err => console.log(err))
+  }
   render() {
     return (
       <LoginWrapper>
         <CenterOfScreen>
           <SwoleLoginLogo/>
         </CenterOfScreen>
-        <TwitterFollowButton />  
+        <TwitterFollowButton login={this.twitterLogin()} />  
       </LoginWrapper>
     )
   }
