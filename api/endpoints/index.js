f0d6ee74c507f4535c4782e2658a10f865237acb
James Andrews <bdaddyja@gmail.com>
2019-04-06 12:51:32 -0700

    Added user to endpoints index

--- api/endpoints/index.js
+++ api/endpoints/index.js
@@ -4,8 +4,10 @@ const router  = express.Router()
 
 const login   = require('./login')
 const workout = require('./workout')
+const user    = require('./user')
 
 router.use('/login', login)
 router.use('/workout', workout)
+router.use('/user', user)
 
 module.exports = router
