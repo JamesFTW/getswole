bf911a1baed51ec4c48376c125351d0529e00a35
James Andrews <bdaddyja@gmail.com>
2019-04-06 12:55:57 -0700

    Fix endpoint

--- api/endpoints/user.js
+++ api/endpoints/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('connect-ensure-login').ensureLoggedIn()
 
-router.get(':user', isLoggedIn, (req, res) => {
+router.get('/:user', isLoggedIn, (req, res) => {
   const { user } = req.params
 
   res.send({ user: user })
