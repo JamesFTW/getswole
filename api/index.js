7b9a5bd5b1f97f68a26102bd1bce13c3a4114ba1
James Andrews <bdaddyja@gmail.com>
2019-04-08 14:05:45 -0700

    Add cors headers

--- api/index.js
+++ api/index.js
@@ -20,6 +20,11 @@ app.use(passport.session())
 app.use(cookieParser)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 app.use('/api', endpoints)
 
 app.listen(port, () => console.log(`listening on ${port}`))
