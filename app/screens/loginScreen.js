72077d818e82825f9e70d801ba659882bd9b7834
James Andrews <bdaddyja@gmail.com>
2019-04-09 13:19:07 -0700

    Added squat photo to login screen

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -6,18 +6,14 @@ import TwitterLoginButton       from '../components/twitterLoginButton.js'
 import SwoleLoginLogo           from '../components/swoleLoginLogo.js'
 
 export default class LoginScreen extends PureComponent {
-  componentDidMount() {
-    console.log('test')
-    //fetch API to check if person already logged in via asyncstorage
+  constructor(props) {
+    super(props)
+    SQUATPHOTO = 'squatphoto'
   }
-  twitterLogin = () => {
-    fetch('https://swole.herokuapp.com/api/login/twitter')
-      .then(res => console.log(res.url))
-      .catch(err => console.log(err))
-  }
+
   render() {
     return (
-      <LoginWrapper>
+      <LoginWrapper photo={SQUATPHOTO}>
         <CenterOfScreen>
           <SwoleLoginLogo/>
         </CenterOfScreen>
