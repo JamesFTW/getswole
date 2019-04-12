5a309a05b9b02ab78f3c96f0d82c96cad6c82585
James Andrews <bdaddyja@gmail.com>
2019-04-11 21:54:32 -0700

    Change status to sendStatus

--- api/endpoints/login.js
+++ api/endpoints/login.js
@@ -8,11 +8,11 @@ router.get('/twitter', authenticate)
 router.get('/oauth/callback', authenticate)
 
 router.get('/success', isLoggedIn, (req, res) => {
-  res.send(200).end()
+  res.sendStatus(200).end()
 })
 
 router.get('/failed', (req, res) => {
-  res.send(401).end()
+  res.sendStatus(401).end()
 })
 
 module.exports = router
