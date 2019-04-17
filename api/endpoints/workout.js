8a80cb1254a7ab3f701166a3f8fb5ca856509305
James Andrews <bdaddyja@gmail.com>
2019-04-16 22:06:58 -0700

    Log session

--- api/endpoints/workout.js
+++ api/endpoints/workout.js
@@ -15,7 +15,7 @@ router.get('/test', isLoggedIn, (req, res) => {
   const { session, cookies } = req
   const result = { session, cookies }
 
-  console.log(session.user)
+  console.log(session)
 
   res.send(result)
 })
