4f0b6717790ee5c95739a11b8722d43f98f8920f
James Andrews <bdaddyja@gmail.com>
2019-04-18 01:34:53 -0700

    Fix exports

--- api/db/queries/index.js
+++ api/db/queries/index.js
@@ -1,5 +1,10 @@
-export const GET_USER = 
+const GET_USER = 
   `SELECT userid, username, profilephoto FROM users WHERE twitterid=$1`
 
-export const CREATE_USER = 
+const CREATE_USER = 
   `INSERT into users (username, profilePhoto) VALUES ($1, $2) RETURNING userid`
+
+  module.exports = {
+    GET_USER,
+    CREATE_USER
+  }
