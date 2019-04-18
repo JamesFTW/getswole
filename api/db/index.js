42422cd328c338c519f603348d7e9c69c094e36e
James Andrews <bdaddyja@gmail.com>
2019-04-17 23:23:12 -0700

    Init user and workout

--- api/db/index.js
+++ api/db/index.js
@@ -3,4 +3,11 @@ const pgp = require('pg-promise')()
 const connection = `${process.env.DATABASE_URL}?ssl=true`
 const db = pgp(connection)
 
-module.exports = { db, connection }
+const userInit    = require('./user.js')
+const workoutInit = require('./workout.js')
+
+module.exports = { 
+  connection,
+  User: userInit(db),
+  Workout: workoutInit(db) 
+}
