77e64831dbeb015a552fed82151230103ecb2e50
James Andrews <bdaddyja@gmail.com>
2019-04-23 16:45:40 -0700

    Create user route

--- api/endpoints/user.js
+++ api/endpoints/user.js
@@ -2,10 +2,20 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('connect-ensure-login').ensureLoggedIn('/api/login')
 
+const { User } = require('../db')
+
 router.get('/', isLoggedIn, (req, res) => {
   const { passport } = req.session
 
   res.json(passport)
+})
+
+router.post('/create', isLoggedIn, (req, res) => {
+  const { username, profilePhoto } = req.body
+
+  User.create(username, profilePhoto)
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
