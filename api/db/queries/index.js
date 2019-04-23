a4396e390582463cb0389096ecedfa3aab6827f3
James Andrews <bdaddyja@gmail.com>
2019-04-23 16:58:14 -0700

    Clean up CREATE_USER db query

--- api/db/queries/index.js
+++ api/db/queries/index.js
@@ -2,7 +2,7 @@ const GET_USER =
   `SELECT userid, username, profilephoto FROM users WHERE twitterid=$1`
 
 const CREATE_USER = 
-  `INSERT into users (username, profilePhoto) VALUES ($1, $2) RETURNING userid`
+  `INSERT INTO users(username, profilePhoto) VALUES($1, $2) RETURNING userid`
 
   module.exports = {
     GET_USER,
