a00305f818ae5053753b30708c0ed17d7654eec8
James Andrews <bdaddyja@gmail.com>
2019-04-29 17:12:00 -0700

    If you user info is cached pull from that and not api

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,17 +1,68 @@
 
-import React, { PureComponent } from 'react'
-import LoginWrapper             from '../components/loginWrapper.js'
-import CenterOfScreen           from '../components/centerOfScreen.js'
-import TwitterLoginButton       from '../components/twitterLoginButton.js'
-import SwoleLoginLogo           from '../components/swoleLoginLogo.js'
+import React, { PureComponent }    from 'react'
+import LoginWrapper                from '../components/loginWrapper.js'
+import CenterOfScreen              from '../components/centerOfScreen.js'
+import TwitterLoginButton          from '../components/twitterLoginButton.js'
+import SwoleLoginLogo              from '../components/swoleLoginLogo.js'
+import Loading                     from '../components/loading'
+import { getUser, getUserSession } from '../api'
+import { Redirect }                from "react-router-native"
 
 export default class LoginScreen extends PureComponent {
   constructor(props) {
-    super(props)
     SQUATPHOTO = 'squatphoto'
+
+    super(props)
+    this.state = {
+      user: '',
+      redirectToWorkoutScreen: false,
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    getUserSession()
+      .then(data => {
+        const userId = parseInt(data.user.id)
+
+        getUser(userId)
+          .then(data => {
+            this.setState({
+              user: data.userid,
+              redirectToWorkoutScreen: true,
+              isLoading: false
+            })
+          })
+      })
+      .catch(_ => {
+        this.setState({
+          isLoading: false
+        })
+      })
   }
 
   render() {
+    const {
+      redirectToWorkoutScreen,
+      user,
+      isLoading
+    } = this.state
+
+    if (isLoading) {
+      return <Loading />
+    }
+
+    if (redirectToWorkoutScreen) {
+      return (
+        <Redirect to={{
+          pathname: '/Workout',
+          state: {
+            user
+          }
+        }} />
+      )
+    }
+
     return (
       <LoginWrapper photo={SQUATPHOTO}>
         <CenterOfScreen>
