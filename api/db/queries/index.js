92f172eb9088a61e40c853fc5e12dc14f12e23d1
James Andrews <bdaddyja@gmail.com>
2019-05-10 02:50:08 -0700

    REGISTER_USERWORKOUT_PLAN query

--- api/db/queries/index.js
+++ api/db/queries/index.js
@@ -1,10 +1,14 @@
 const GET_USER = 
   `SELECT userid, username, profilephoto FROM users WHERE twitterid=$1`
-
+//Eventually change this query to only send stuff I need to render on frontend
 const CREATE_USER = 
   `INSERT INTO users(username, profilePhoto, twitterid) VALUES($1, $2, $3) RETURNING *`
 
+const REGISTER_USERWORKOUT_PLAN =
+  `INSERT INTO userworkoutplans(userid, planid, createdat, endat) VALUES($1, $2, $3, $4)`
+
   module.exports = {
     GET_USER,
-    CREATE_USER
+    CREATE_USER,
+    REGISTER_USERWORKOUT_PLAN
   }
--- api/db/user.js
+++ api/db/user.js
@@ -1,5 +1,9 @@
 
-const { GET_USER, CREATE_USER } = require('./queries')
+const {
+  GET_USER,
+  CREATE_USER,
+  REGISTER_USERWORKOUT_PLAN
+} = require('./queries')
 
 const findById = db => twitterid =>
   db.one(GET_USER, twitterid)
@@ -7,13 +11,18 @@ const findById = db => twitterid =>
 const createUser = db => (username, profilePhoto, twitterid) =>
   db.one(CREATE_USER, [username, profilePhoto, twitterid])
 
+const createUserPlan = db => (userId, planId, startDate, endDate) => {
+  db.one(REGISTER_USERWORKOUT_PLAN, userId, planId, startDate, endDate)
+}
+
 const getUserWorkouts = db => id =>
   'too be added'
 
 const userInit = db => ({
   findById: findById(db),
   create: createUser(db),
-  getWorkouts: getUserWorkouts(db)
+  getWorkouts: getUserWorkouts(db),
+  createWorkoutPlan: createUserPlan(db)
 })
 
 module.exports = userInit
