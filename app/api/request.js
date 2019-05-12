d7be4a4b1d67e316b4923959f9390eab90baaf13
James Andrews <bdaddyja@gmail.com>
2019-05-12 08:51:14 -0700

    Add credentials to request and constants

--- app/api/request.js
+++ app/api/request.js
@@ -1,15 +1,20 @@
 
-const request = ({ endpoint, body, method = 'POST', headers }) => {
+const SAME_ORIGIN = 'same-origin'
+const CONTENT_TYPE = 'Content-Type'
+const POST = 'POST'
+
+const request = ({ endpoint, body, method = POST, headers }) => {
   const newHeaders = new Headers()
 
   if (headers) {
-    newHeaders.append('Content-Type', headers)
+    newHeaders.append(CONTENT_TYPE, headers)
   }
 
   return (
     fetch(endpoint, {
       method: method,
       headers: newHeaders,
+      credentials: SAME_ORIGIN,
       body: body
     })
   )
