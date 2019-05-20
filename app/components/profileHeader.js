aea2d72ebe76a2fc9d80eb96e5bb5ebe35f1dd5c
James Andrews <bdaddyja@gmail.com>
2019-05-19 17:44:30 -0700

    Change profileHeader color

--- app/components/profileHeader.js
+++ app/components/profileHeader.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     height: 55,
     alignContent: 'center',
     justifyContent: 'center',
-    backgroundColor: '#9A87D1',
+    backgroundColor: '#879ACB',
   },
   header: {
     zIndex: 3,
