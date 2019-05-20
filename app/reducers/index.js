365efda2e681cdbe4b78fa6024de9d131c548fd9
James Andrews <bdaddyja@gmail.com>
2019-05-19 17:38:10 -0700

    Combine reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import weightCounter from './weightCounter.js'
 import followReducer from './followReducer.js'
+import workoutHeaderReducer from './workoutHeaderReducer'
 
 export const rootReducer = combineReducers({
   weightCounter,
-  followReducer
+  followReducer,
+  workoutHeaderReducer
 })
