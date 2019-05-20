387dc5da8e7649477d4a3a41b12e1119e0e9c4a8
James Andrews <bdaddyja@gmail.com>
2019-05-19 21:52:53 -0700

    Clean up reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux'
-import weightCounter from './weightCounter.js'
-import followReducer from './followReducer.js'
+import { combineReducers }  from 'redux'
+import weightCounter        from './weightCounter.js'
+import followReducer        from './followReducer.js'
 import workoutHeaderReducer from './workoutHeaderReducer'
 
 export const rootReducer = combineReducers({
