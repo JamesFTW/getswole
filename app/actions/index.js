4f77024f0c65a70bb21b17e08160421176391bd7
James Andrews <bdaddyja@gmail.com>
2019-05-19 17:36:30 -0700

    toggleHeaderWorkout and toggleHeaderStats actions

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,6 +2,8 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 export const TOGGLE_FOLLOW = 'TOGGLE_FOLLOW'
+export const TOGGLE_HEADER_WORKOUT = 'TOOGLE_HEADER_WORKOUT'
+export const TOGGLE_HEADER_STATS = 'TOOGLE_HEADER_STATS'
 
 export const increment = nodeId => ({
   type: INCREMENT,
@@ -15,5 +17,13 @@ export const decrement = nodeId => ({
 
 export const toggleFollow = () => ({
   type: TOGGLE_FOLLOW
+})
+
+export const toggleHeaderWorkout = () => ({
+  type: TOGGLE_HEADER_WORKOUT
+})
+
+export const toggleHeaderStats = () => ({
+  type: TOGGLE_HEADER_STATS
 })
 
