9b9a96bb936d60212a6f9281eb286afc8908453b
James Andrews <bdaddyja@gmail.com>
2019-05-29 12:50:17 -0700

    add statsExerciseContainer

---
+++ app/components/statsExerciseContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import StatsExercise        from '../components/statsExercise'
+
+import {
+  View,
+  StyleSheet,
+} from 'react-native'
+
+const bodyParts = [
+  'Arms',
+  'Legs',
+  'Shoulders',
+  'Back',
+  'Abs'
+]
+
+export default class StatsExerciseContainer extends Component {
+  render() {
+    const ExerciseStats = bodyParts.map((exercise, i) => (
+      <StatsExercise key={i.toString()} bodyPart={exercise} />
+    ))
+
+    return (
+      <View style={styles.container}>
+        { ExerciseStats }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    backgroundColor: 'white',
+    paddingLeft: 20,
+    paddingBottom: 25,
+  }
+})
