6d7ff1e0a6d864c9bcce32acdf0b0fcc620fe16d
James Andrews <bdaddyja@gmail.com>
2019-06-04 16:38:14 -0700

    userWorkout endpoint

---
+++ api/endpoints/userWorkout.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+const isLoggedIn = require('connect-ensure-login').ensureLoggedIn('/api/login')
+
+const { UserWorkoutPlan } = require('./db')
+
+//check if user has completed any workouts
+//starting new userworkouts
+
+router.post('/create', isLoggedIn, (req, res) => {
+  //creation of userworkouts
+  const {
+    userId,
+    planId,
+    length,
+    timeStamp
+  } = req.body
+
+  const oneWeek = 604800000
+  const endDateTimeStamp = (length * oneWeek) + timeStamp
+
+  const startDate = new Date(timeStamp)
+  const endDate = new Date(endDateTimeStamp) // have to change this to 11:59pm
+
+  UserWorkoutPlan.create(userId, planId, startDate, endDate)
+    .then(_ => res.sendStatus(200))
+    .catch(err => console.log(err))
+})
+
+/**
+ * /userworkout/create //creates workoutplan for user
+ * /userworkout/:id //returns current userworkout
+ * /userworkout/edit //changes workoutplan if user already has one
+ */
