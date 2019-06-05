c744104c8f1e889c6ef4b3ae9067fd488da12bb4
James Andrews <bdaddyja@gmail.com>
2019-06-05 00:57:20 -0700

    fix path to db

--- api/endpoints/userWorkout.js
+++ api/endpoints/userWorkout.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('connect-ensure-login').ensureLoggedIn('/api/login')
 
-const { UserWorkoutPlan } = require('./db')
+const { UserWorkoutPlan } = require('../db')
 
 //check if user has completed any workouts
 //starting new userworkouts
