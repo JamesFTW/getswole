f1b543ff844328c2444a06c9a52d68f703874746
James Andrews <bdaddyja@gmail.com>
2019-06-14 20:15:09 -0700

    userwork findByUserid method

--- api/db/userWorkout.js
+++ api/db/userWorkout.js
@@ -1,13 +1,19 @@
 
 const {
-  REGISTER_USERWORKOUT_PLAN
+  REGISTER_USERWORKOUT_PLAN,
+  FIND_USERWORKOUT_PLAN
 } = require('./queries')
 
 const createUserPlan = db => (userId, planId, startDate, endDate) =>
   db.none(REGISTER_USERWORKOUT_PLAN, [userId, planId, startDate, endDate])
 
+const findUserPlan = db => userid => (
+  db.any(FIND_USERWORKOUT_PLAN, userid)
+)
+
 const userWorkoutPlanInit = db => ({
-  create: createUserPlan(db)
+  create: createUserPlan(db),
+  findByUserid: findUserPlan(db)
 })
 
 module.exports = userWorkoutPlanInit
