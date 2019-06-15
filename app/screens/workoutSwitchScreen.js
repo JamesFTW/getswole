50847cea5bd608b61f32b978dc745208127eddad
James Andrews <bdaddyja@gmail.com>
2019-06-14 20:21:10 -0700

    Clean up workoutSwitchScreen

--- app/screens/workoutSwitchScreen.js
+++ app/screens/workoutSwitchScreen.js
@@ -2,15 +2,37 @@
 import React, { Component } from 'react'
 import BackGroundWrapper    from '../components/backGroundWrapper.js'
 import Header               from '../components/header.js'
+import Loading              from '../components/loading'
 import WorkoutContainer     from '../containers/workoutContainer.js'
 import StatsScreen          from '../screens/statsScreen.js'
 import { connect }          from 'react-redux'
 
 class WorkoutSwitchScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      isLoading: false
+    })
+    /**
+     * here is where you check if you has a current workoutplan.
+     * if so show workout of the day or next workout.
+     * if not show workoutSelectScreen
+     */
+  }
   render() {
+    const { isLoading } = this.state
     const {
       workoutSelected
     } = this.props
+
+    if(isLoading) {
+      return <Loading/>
+    }
 
     const SwitchContent = workoutSelected? <WorkoutContainer /> : <StatsScreen />
     const workoutName = workoutSelected? "Leg Day" : "Exercise Stats"
