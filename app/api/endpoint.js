3aa1698fdb0414c414f2a36eae0dd2548d148621
James Andrews <bdaddyja@gmail.com>
2019-06-16 22:31:18 -0700

    Set up local server

--- app/api/endpoint.js
+++ app/api/endpoint.js
@@ -1,1 +1,10 @@
-export let API_ENDPOINT = 'https://swole.herokuapp.com/api'
+
+let API_ENDPOINT = ''
+
+if (process.env.NODE_ENV === 'development') {
+  API_ENDPOINT = 'http://192.168.1.172:3000/api'
+} else {
+  API_ENDPOINT = 'https://swole.herokuapp.com/api'
+}
+
+module.exports = { API_ENDPOINT }
--- config/twitterConfig.js
+++ config/twitterConfig.js
@@ -1,8 +1,10 @@
 
+const { API_ENDPOINT } = require('../app/api/endpoint.js')
+
 const twitterConfig = {
   consumerKey: process.env.TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.TWITTER_SECRET_KEY,
-  callbackURL: "https://swole.herokuapp.com/api/login/oauth/callback"
+  callbackURL: `${API_ENDPOINT}/login/oauth/callback`
 }
 
 module.exports = twitterConfig
