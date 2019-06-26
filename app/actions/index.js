4abbd44450879c4f744d0b8c7b5a10f19561cfcc
James Andrews <bdaddyja@gmail.com>
2019-06-26 16:56:46 -0700

    Add fetchData and fetchWorkouts actions

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,9 +1,12 @@
 
+import { getUserWorkoutPlan } from '../api'
+
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 export const TOGGLE_FOLLOW = 'TOGGLE_FOLLOW'
 export const TOGGLE_HEADER_WORKOUT = 'TOOGLE_HEADER_WORKOUT'
 export const TOGGLE_HEADER_STATS = 'TOOGLE_HEADER_STATS'
+export const FETCH_DATA = 'FETCH_DATA'
 
 export const increment = nodeId => ({
   type: INCREMENT,
@@ -27,3 +30,17 @@ export const toggleHeaderStats = () => ({
   type: TOGGLE_HEADER_STATS
 })
 
+export const fetchData = data => {
+  return {
+    type: FETCH_DATA,
+    data
+  }
+}
+
+export const fetchWorkouts = () => {
+  return (dispatch) => {
+    return getUserWorkoutPlan()
+      .then(res => dispatch(fetchData(res)))
+      .catch(err => console.log(err))
+  }
+}
